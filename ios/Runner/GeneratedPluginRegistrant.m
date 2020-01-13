b600a09d5c744eb7cb0839253ffd5aeb1c39eab2
malek.slimani <malek.slimani@vistex.com>
2020-01-13 13:10:30 +0100

    First Commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <cloud_firestore/CloudFirestorePlugin.h>
+#import <contact_picker/ContactPickerPlugin.h>
+#import <firebase_auth/FirebaseAuthPlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
+#import <firebase_storage/FirebaseStoragePlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTCloudFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTCloudFirestorePlugin"]];
+  [ContactPickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"ContactPickerPlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseStoragePlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
